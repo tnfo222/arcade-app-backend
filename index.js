1ec441ee2bebf1c20fdcfcbc3eb5b04de81643f5
tnfo222 <tayforbes92@hotmail.com>
2022-11-26 17:45:54 -0500

    Changed BE to postgres. Migrations and models created

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-require('dotenv').config()
+const { Sequelize } = require('sequelize')
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const cookieSession = require('cookie-session')
+const defineCurrentUser = require('./middleware/defineCurrentUser')
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
+//Configuration / Middleware
+require('dotenv').config()
+
+// Express Settings
+app.use(cookieSession({
+    name: 'session',
+    keys: [ process.env.SESSION_SECRET ],
+    sameSite: 'strict',
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}))
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(defineCurrentUser)
+
+// Controllers & Routes
+
+app.use(express.urlencoded({ extended: true }))
+app.use('/users', require('./controllers/users'))
+app.use('/authentication', require('./controllers/authentication'))
+
+// Listen for Connections
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on ${process.env.PORT}`)
 })
-
-app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
-})
-
-app.listen(process.env.PORT)
-
